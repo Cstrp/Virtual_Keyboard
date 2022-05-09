e99ec0b8c1d33fc451b29da52b53aaea50544114
Valery Nosareu <86977555+Cstrp@users.noreply.github.com>
2022-05-09 18:43:39 +0300

    delete comm

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,4 @@
-import '../style/index.scss';
-import Keyboard from './keyboard';
+import '../style/index.scss'
+import Keyboard from './keyboard'
 
-new Keyboard().render();
-
-// const arr = []
-// document.addEventListener('keydown', (e) => {
-//   arr.push(e.code)
-//   console.log(arr)
-// })
+new Keyboard().render()
--- src/js/sup.js
+++ src/js/sup.js
@@ -11,41 +11,31 @@ const dd = dt.data
 const dl = dt.width * dt.height
 
 function generateNoise() {
-  let p = 0
-  let i = 0
-  for (; i < dl; ++i) {
-    dd[p++] = c = Math.floor(Math.random() * 100)
-    dd[p++] = c
-    dd[p++] = c
-    dd[p++] = 325
-  }
-  cx.putImageData(dt, 0, 0)
+    let p = 0
+    let i = 0
+    for (; i < dl; ++i) {
+        dd[p++] = c = Math.floor(Math.random() * 100)
+        dd[p++] = c
+        dd[p++] = c
+        dd[p++] = 325
+    }
+    cx.putImageData(dt, 0, 0)
 }
 
 function resize() {
-  const w = window.innerWidth
-  const h = window.innerHeight
-  background_w = background.width = w >> 1
-  background_h = background.height = h >> 1
-  background.style.width = `${w}px`
-  background.style.height = `${h}px`
+    const w = window.innerWidth
+    const h = window.innerHeight
+    background_w = background.width = w >> 1
+    background_h = background.height = h >> 1
+    background.style.width = `${w}px`
+    background.style.height = `${h}px`
 }
 
 resize()
 window.addEventListener('resize', resize, false)
-// window.addEventListener(
-//   "load",
-//   function () {
-//     generateNoise();
-//     background_cx.fillStyle = background_cx.createPattern(cv, "repeat");
-//     background_cx.fillRect(0, 0, background_w, background_h);
-//     setTimeout(arguments.callee, 15);
-//   },
-//   false
-// ); // жесткий вариант горения компов
 window.requestAnimationFrame(function () {
-  generateNoise()
-  background_cx.fillStyle = background_cx.createPattern(cv, 'repeat')
-  background_cx.fillRect(0, 0, background_w, background_h)
-  window.requestAnimationFrame(arguments.callee)
+    generateNoise()
+    background_cx.fillStyle = background_cx.createPattern(cv, 'repeat')
+    background_cx.fillRect(0, 0, background_w, background_h)
+    window.requestAnimationFrame(arguments.callee)
 })
